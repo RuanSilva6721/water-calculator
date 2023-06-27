5cb27db1320c743a7a14dcd98ed51935483a0710
Ruan Felipe <ruansilva6721@gmail.com>
2023-06-27 13:54:36 -0300

    setup init

--- App.js
+++ App.js
@@ -1,20 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet, Image } from 'react-native';
 
-export default function App() {
+const WaterCalculator = () => {
+  const [totalWater, setTotalWater] = useState(0);
+
+  const addWater = (amount) => {
+    setTotalWater(totalWater + amount);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>
+        Água é Saúde
+      </Text>
+
+      <Image
+        source={require('./assets/ZeGotinha.jpeg')}
+        style={styles.image}
+      />
+      <Text style={styles.textWater}>
+        Clique no bõtão abaixo de acordo com a quantidade de ml consumida
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="200ml"
+          onPress={() => addWater(200)}
+        />
+
+        <View style={styles.buttonSpacer} />
+
+        <Button
+          title="500ml"
+          onPress={() => addWater(500)}
+        />
+
+        <View style={styles.buttonSpacer} />
+
+        <Button
+          title="700ml"
+          onPress={() => addWater(700)}
+        />
+      </View>
+
+      <Text style={styles.totalWater}>
+        Consumo: {totalWater / 1000} litros
+      </Text>
+      <Text style={styles.totalWater}>
+        Ruan Felipe Silva Negrão
+      </Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    padding: 16,
+  },
+  totalWater: {
+    fontSize: 20,
+    marginBottom: 10,
+    marginTop: 30,
+    fontWeight: 'bold'
+  },
+  textWater: {
+    fontSize: 20,
+    marginBottom: 10,
+    marginTop: 30
+  },
+  buttonContainer: {
+    flexDirection: 'row',
     justifyContent: 'center',
+    marginTop: 10,
+  },
+  buttonSpacer: {
+    width: 10,
+  },
+  title: {
+    fontSize: 30,
+    marginTop: 30,
+    fontWeight: 'bold',
+    backgroundColor: '#1e90ff',
+    color: '#fff',
+    textAlign: 'center',
+    width: '110%',
+    padding: 10,
+  },
+  image: {
+    width: 500,
+    height: 250,
+    resizeMode: 'contain',
+    marginBottom: 20,
   },
 });
+
+export default WaterCalculator;
